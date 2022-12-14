ef425b09d87500880e672039e84a9006a9a93962
ZHY-CD <1170327086@qq.com>
2022-12-14 15:51:40 +0800

    配置Mysql与注册登录模块（下）

--- web/src/views/user/account/UserAccountLoginView.vue
+++ web/src/views/user/account/UserAccountLoginView.vue
@@ -35,6 +35,22 @@ export default {
         let password = ref('');
         let error_message = ref('');
 
+        const jwt_token = localStorage.getItem("jwt_token");
+        if (jwt_token) {
+            store.commit("updateToken", jwt_token);
+            store.dispatch("getinfo", {
+                success() {
+                    router.push({ name: "home" });
+                    store.commit("updatePullingInfo", true);
+                },
+                error() {
+                    store.commit("updatePullingInfo", false);
+                }
+            })
+        } else {
+            store.commit("updatePullingInfo", false);
+        }
+
         const login = () => {
             error_message.value = "";
             store.dispatch("login", {
